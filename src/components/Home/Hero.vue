bdbe4955d58c8331253d4197e6458190fb201a2b
muras <murashov.temka@gmail.com>
2023-03-22 16:17:24 +0300

    first commit

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="hero">
+  <section class="hero">
     <Header/>
-  </div>
+    <div class="info">
+        <div class="info__title">
+            Introduce Your Product <br>
+            Quickly & Effectively
+        </div>
+        <div class="info__text">
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus</p>
+            <p>mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+        </div>
+        <div class="info__buttons">
+
+        </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -13,12 +27,27 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
   .hero {
     height: 100vh;
     background-image: url("src/assets/designer.png");
     background-repeat: no-repeat;
     background-size: contain;
     background-position: right;
+  }
+  .info {
+      margin-left: 15.5vw;
+      margin-top: 15vh;
+      width: 35vw;
+      .info__title {
+          font-size: 50px;
+          color: #091133;
+          font-weight: 500;
+          margin-bottom: 20px;
+      }
+      .info__text {
+          font-size: 18px;
+          color: #505F98;
+      }
   }
 </style>
