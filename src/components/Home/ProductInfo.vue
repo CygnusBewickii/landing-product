c26fc05f3f8556bf5cdd3380e6bc51ed3e211ad6
muras <murashov.temka@gmail.com>
2023-03-23 13:01:39 +0300

    commit

---
+++ src/components/Home/ProductInfo.vue
@@ -0,0 +1,73 @@
+<template>
+    <section>
+        <div class="wrapper" :style="{ 'background-image': 'url('+imageUrl+')' }">
+            <div class="container">
+                <div class="content">
+                    <div class="info">
+                        <div class="info__title">
+                            {{title}}
+                        </div>
+                        <div class="info__text">
+                            <p>{{text}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "ProductInfo",
+    props: {
+        imageUrl: String,
+        text: String,
+        title: String
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+    height: 100vh;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: left;
+}
+
+.container {
+    margin: 0 10vw;
+}
+
+.content {
+    padding: 20% 0;
+    width: 35vw;
+    float: right;
+}
+
+.info {
+    margin-bottom: 40px;
+
+    .info__title {
+        font-weight: 500;
+        font-size: 36px;
+        color: #091133;
+        margin-bottom: 30px;
+    }
+
+    .info__text {
+        font-size: 16px;
+        color: #6F7CB2;
+        line-height: 26px;
+
+        p {
+            margin-bottom: 15px;
+
+            &:last-child {
+                margin-bottom: 0;
+            }
+        }
+    }
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,16 +2,26 @@
     <div class="container">
         <Hero/>
         <MainContent/>
+        <ProductInfo :text="text" :title="title" imageUrl="src/assets/Lotos.svg"/>
     </div>
 </template>
 
 <script>
 import Hero from "../components/Home/Hero.vue";
 import MainContent from "../components/Home/MainContent.vue";
+import ProductInfo from "../components/Home/ProductInfo.vue";
 
 export default {
     name: "Home",
-    components: {MainContent, Hero}
+    components: {ProductInfo, MainContent, Hero},
+    data() {
+        return {
+            text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus \n" +
+                "mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.",
+            title: "Light, Fast & Powerful",
+
+        }
+    }
 }
 </script>
 
